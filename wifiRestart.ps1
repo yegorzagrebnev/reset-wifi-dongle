fb4b56a5233f0b1b76e71dab2f42a6da035273d6
Yegor Zagrebnev <yegorzagrebnev@tutanota.com>
2024-12-01 01:05:04 +0500

    Initial commit

---
+++ wifiRestart.ps1
@@ -0,0 +1,13 @@
+$adapterName = "Wi-Fi"
+
+$adapter = Get-NetAdapter | Where-Object {$_.Name -eq $adapterName}
+
+if ($adapter) {
+    Disable-NetAdapter -Name $adapter.Name -Confirm:$false
+    
+    Start-Sleep -Seconds 3
+
+    Enable-NetAdapter -Name $adapter.Name -Confirm:$false
+} else {
+    Write-Host "Adapter '$adapterName' not found!"
+}
